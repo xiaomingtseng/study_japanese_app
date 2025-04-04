8eb6857c24e699fe61f52766e3e718a5bab55815
曾健智 <124895280+xiaomingtseng@users.noreply.github.com>
2025-04-05 01:43:01 +0800

    [chore] write some basics give an app entrance view

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,51 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(title: const Text('日文小幫手')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            //title area
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                '歡迎來到日語學習應用',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            //content area
+            Expanded(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      '這裡是內容區域',
+                      style: TextStyle(fontSize: 20),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 20),
+                    Icon(
+                      Icons.school,
+                      size: 50,
+                      color: Color.fromARGB(0, 7, 85, 187),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            //button area
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('開始學習'),
+              ),
+            ),
+          ],
         ),
       ),
     );
